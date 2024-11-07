2f455bfd90f31a916d4852f1fe3461aa7b053384
hiroshi <hiroshi@tanuki.page>
2024-11-07 02:20:28 -0500

    hardware/xiaomi -> hardware/xiaomi-mtk
    Change-Id: I1acebcd81769df0b8d1e4d1f1bf99509f1a0d3c1

--- interfaces/updates-makefiles.sh
+++ interfaces/updates-makefiles.sh
@@ -3,7 +3,7 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.goodix:hardware/xiaomi/interfaces/goodix"
+  "vendor.goodix:hardware/xiaomi-mtk/interfaces/goodix"
 
 do_makefiles_update \
-  "vendor.xiaomi:hardware/xiaomi/interfaces/xiaomi"
+  "vendor.xiaomi:hardware/xiaomi-mtk/interfaces/xiaomi"
